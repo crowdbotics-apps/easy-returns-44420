5d44cabba4bb120aa945692204b4ecb050110d11
Crowdbotics <team@crowdbotics.com>
2023-11-08 18:26:56 +0000

    Initial commit

---
+++ backend/easy_returns_44420/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for easy_returns_44420 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "easy_returns_44420.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "easy-returns-44420",
+  url: "https://easy-returns-44420.botics.co",
+  api: "https://easy-returns-44420.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
